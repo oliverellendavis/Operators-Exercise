70ce1ba65f2b8d08b30dfa540ab050b181496f4d
oliverellendavis <oliverellendavis@gmail.com>
2024-09-29 15:20:52 -0400

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace OperatorExercise
+﻿using System;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            var sum = a + b;
+            var difference = a - b;
+            var product = a * b;
+            var quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"Addition: {a} + {b} = {sum}");
+            Console.WriteLine($"Subtraction: {a} - {b} = {difference}");
+            Console.WriteLine($"Multiplication: {a} * {b} = {product}");
+            Console.WriteLine($"Division: {a} / {b} = {quotient} remainder {remainder}");
+            Console.WriteLine($"Modulus: {a} % {b} = {remainder}");
+
+            Console.Write("What is the radius of your circle? ");
+            var radius = double.Parse(Console.ReadLine());
+
+            double area = AreaOfCircle(radius);
+
+            Console.WriteLine($"The area of the circle with radius {radius} is {area}");
         }
+
+        static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
+        }        
     }
 }
